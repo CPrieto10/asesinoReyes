b4921e93037af5418dd3c9744ee371bf2242a6ef
Carlos <carlos_prieto10@hotmail.com>
2025-05-27 11:00:39 +0200

    Proyecto completado

--- index.php
+++ index.php
@@ -34,7 +34,11 @@ $libros = $pdo->query('SELECT * FROM libros')->fetchAll();
 </section>
 
 <footer class="parallax parallax-footer">
-  <p>© 2025 Carlos Prieto | Proyecto educativo | <a href="aviso.php">Aviso legal y política de privacidad</a></p>
+  <p>© 2025 Carlos Prieto | Proyecto educativo | <a href="aviso.php" class = "link-legal">Aviso legal y política de privacidad</a> | 
+  <a href="https://github.com/CPrieto10" target="_blank" class="github-link" title="Mi GitHub">
+      <img src="imagenes/github-icon.png" alt="GitHub" class="github-icon">
+    </a>
+  </p>
 </footer>
 
 </body>
